cc6950fe4a45ddefbfc4f7ef3c63f8962c67100f
Milo504 <chavez100577@gmail.com>
2026-07-29 23:38:42 -0600

    Recipes Balancing

--- BP/scripts/Config/recipes/infuser.js
+++ BP/scripts/Config/recipes/infuser.js
@@ -1,9 +1,10 @@
 import * as DoriosLib from "DoriosLib/index.js";
 
 const infuserRecipes = {
-    "utilitycraft:diamond_dust|minecraft:amethyst_shard": {
+    "utilitycraft:diamond_dust|minecraft:quartz": {
         output: "utilitycraft:fluxite",
-        required: 2,
+        required: 1,
+        input_required: 1,
         amount: 1
     }
 };
